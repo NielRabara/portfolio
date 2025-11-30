86e47358c3b21f9b54f169429a5bcd8609e1ffe0
NielR <nrRabara@mcm.edu.ph>
2025-11-30 23:40:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
+
+const contactBtn = document.getElementById('contact-btn');
+const contactPopup = document.getElementById('contact-popup');
+const closeBtn = document.querySelector('.close-btn');
+
+contactBtn.addEventListener('click', () => {
+    contactPopup.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    contactPopup.style.display = 'none';
+});
+
+contactPopup.addEventListener('click', (e) => {
+    if (e.target === contactPopup) {
+        contactPopup.style.display = 'none';
+    }
+});
